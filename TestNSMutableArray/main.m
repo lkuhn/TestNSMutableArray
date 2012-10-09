062ee653861a48847c25a0d94b9995f1059bfb7b
Lino Kuhn <lkuhn@5tfu-Machine.local>
2012-10-09 23:51:25 +0200

    Initial Commit

---
+++ TestNSMutableArray/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestNSMutableArray
+//
+//  Created by Lino Kuhn on 09.10.12.
+//  Copyright (c) 2012 Lino Kuhn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
